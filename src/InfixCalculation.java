e2391d071d43abd13a4a5e652132181834333f34
ankit15697 <hackerankit5@gmail.com>
2020-02-24 16:31:48 +0530

    Better input stream has been taken

--- src/InfixCalculation.java
+++ src/InfixCalculation.java
@@ -1,15 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 import com.expression.controller.*;
 // This is main class which will be responsible for handling the input expression and will give control to the controller class
-public class InfixCalculation {
+public class InfixCalculation  {
 
     // Main function begins from here
-    public  static void main (String args[]){
+    public  static void main (String args[]) throws IOException {
         // input will be taken from the user
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter input Expression !!!");
-        String expression = sc.nextLine();
-
+        System.out.println("Enter Expression ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String expression = reader.readLine();
         // Controller object is being created here
         ExpressionCalculation exp = new ExpressionCalculation(expression);
         exp.performCalculation();
