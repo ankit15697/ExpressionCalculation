63df8989dee9a18a7c5c43d42dd3de390b6aa000
ankit15697 <hackerankit5@gmail.com>
2020-02-25 14:08:13 +0530

    IOException

--- src/InfixCalculation.java
+++ src/InfixCalculation.java
@@ -3,22 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 import com.expression.controller.*;
-import javafx.application.Preloader;
 
 // This is main class which will be responsible for handling the input expression and will give control to the controller class
 public class InfixCalculation  {
 
     // Main function begins from here
-    public  static void main (String args[]) throws IOException {
+    public  static void main (String args[])  {
         int numberOfexpressions;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number of expressions want to evaluate : ");
         numberOfexpressions = sc.nextInt();
         while(numberOfexpressions--!= 0) {
             // input will be taken from the user
+            String expression="";
             System.out.println("Enter Expression ");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            String expression = reader.readLine();
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                expression = reader.readLine();
+            }
+            catch (IOException e){
+                e.getMessage();
+            }
+
             // Controller object is being created here
             ExpressionCalculation exp = new ExpressionCalculation(expression);
             exp.performCalculation();
